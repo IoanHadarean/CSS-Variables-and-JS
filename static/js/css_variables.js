11d4d8c9a87e708e1f9160127dbe788debf92a40
Ioan Hadarean <hadareannelutu@yahoo.com>
2019-03-24 22:42:45 +0000

    Added script for updating css variables

--- static/js/css_variables.js
+++ static/js/css_variables.js
@@ -1,2 +1,11 @@
 const inputs = document.querySelectorAll('.controls input');
-console.log(inputs);
+
+function handleUpdate() {
+    // gets the dataset sizing (either px or none)
+    const suffix = this.dataset.sizing || '';
+    // Gets the HTML element and updates the property of the css variable with the value
+    document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
+}
+
+inputs.forEach(input => input.addEventListener('change', handleUpdate));
+inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
